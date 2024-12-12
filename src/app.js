45f1520fafc4f1b7e12c7cdf8a4b82b42d0cdb65
RO-shan18 <mehraroshan1802@gmail.com>
2024-12-12 13:27:20 +0530

    HTTP Methods and advanced routing

--- src/app.js
+++ src/app.js
@@ -2,18 +2,73 @@ const express = require("express");
 
 const app = express();
 
-app.use("/test", (req, res)=>{
-    res.send("Hello from the test");
-})
+//HTTP METHODS
 
-app.use("/hello",(req, res)=>{
+app.get("/hello",(req, res)=>{
     res.send("Hello Hello Hello");
 })
 
-//If we put this app.use on top of our code then this will not let u to route different endpoints
-app.use("/",(req, res)=>{
-    res.send("Hello World!!");
+app.post("/user", (req, res)=>{
+    res.send("Data post successfully");
 })
+
+app.put("/user", (req, res)=>{
+    res.send("Data Update successfully");
+})
+
+app.delete("/user", (req, res)=>{
+    res.send("Data Deleted successfully");
+})
+
+app.patch("/user", (req, res)=>{
+    res.send("Data Updated in patch.....");
+})
+
+//different routes
+/* app.use("/ab+c", (req, res)=>{
+    res.send("ABC inside the Server.....");
+})
+ */
+
+/* app.use("/ab*c", (req, res)=>{
+    res.send("ABC inside the Server.....")
+}) */
+
+
+/* app.use("/a(bc)+d", (req, res)=>{
+    res.send("ABC inside the Server.....");
+}) */
+
+ app.use("/ab?d", (req, res)=>{
+    res.send("ABC inside the Server.....");
+}) 
+
+/* app.get(/a/, (req, res)=>{
+    res.send("ABC inside the Server.....");
+}) */
+
+/* app.get(/.*full$/, (req, res)=>{
+    res.send("ABC inside the Server.....");
+}) */
+
+//user query
+app.get("/user", (req, res)=>{
+    res.send("ABC inside the Server.....");
+    console.log(req.query);
+}) 
+
+//user query through routes
+app.get("/userdata/:userid/:username/:userpassword", (req, res)=>{
+    res.send("User data send successfully.....")
+    console.log(req.params)
+})
+
+
+
+//If we put this app.use on top of our code then this will not let u to route different endpoints
+/* app.use("/",(req, res)=>{
+    res.send("Hello World!!");
+}) */
 
 //Any incoming req. from outside will listen this port 
 app.listen(7777, ()=>{
